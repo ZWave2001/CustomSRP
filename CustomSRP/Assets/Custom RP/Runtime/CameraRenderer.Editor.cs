569357d75eeafa5ebbfa692332e30ff3b732f80a
WAVE <O.o>
2025-02-19 06:11:19 +0800

    3.4 Drawing Gizmos - 4.4 Clear Flags

--- CustomSRP/Assets/Custom RP/Runtime/CameraRenderer.Editor.cs
+++ CustomSRP/Assets/Custom RP/Runtime/CameraRenderer.Editor.cs
@@ -1,11 +1,22 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Profiling;
 using UnityEngine.Rendering;
 
 public partial class CameraRenderer
 {
+    partial void DrawGizmos();
     partial void DrawUnsupportedShaders ();
+
+    partial void PrepareForSceneWindow();
+    
+    partial void PrepareBuffer();
+
     
 #if UNITY_EDITOR
+    private string SampleName { get; set; }
+
+    
     private static ShaderTagId[] _legacyShaderTagIds =
     {
         new ShaderTagId("Always"),
@@ -18,6 +29,15 @@ public partial class CameraRenderer
 
     private static Material _errorMaterial;
 
+
+    partial void DrawGizmos()
+    {
+        if (Handles.ShouldRenderGizmos())
+        {
+            context.DrawGizmos(camera, GizmoSubset.PreImageEffects);
+            context.DrawGizmos(camera, GizmoSubset.PostImageEffects);
+        }
+    }
 
     partial void DrawUnsupportedShaders()
     {
@@ -38,5 +58,25 @@ public partial class CameraRenderer
         var filteringSettings = FilteringSettings.defaultValue;
         context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
     }
+
+
+    partial void PrepareForSceneWindow()
+    {
+        if(camera.cameraType == CameraType.SceneView)
+        {
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+    }
+
+    partial void PrepareBuffer()
+    {
+        //camera.name will allocate memory
+        Profiler.BeginSample("Editor Only");
+        _buffer.name = SampleName =  camera.name;
+        Profiler.EndSample();
+    }
+#else
+    const string SampleName = _bufferName;
+    
 #endif
 }
--- CustomSRP/Assets/Custom RP/Runtime/CameraRenderer.cs
+++ CustomSRP/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -11,17 +11,16 @@ public partial class CameraRenderer
 
     private const string _bufferName = "Render Camera";
 
-    private CommandBuffer _buffer = new CommandBuffer()
+    private CommandBuffer _buffer = new()
     {
         name = _bufferName
     };
+
     private CullingResults _cullingResults;
-    
+
     private static ShaderTagId _unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
 
-   
-    
-    
+
     /// <summary>
     /// Draw all geometry that camera can see  
     /// </summary>
@@ -30,49 +29,52 @@ public partial class CameraRenderer
         this.context = context;
         this.camera = camera;
 
+        PrepareBuffer();
+        PrepareForSceneWindow();
+
         if (!Cull())
             return;
-        
+
         Setup();
         DrawVisibleGeometry();
         DrawUnsupportedShaders();
+        DrawGizmos();
         Submit();
     }
-    
+
     void Setup()
     {
         // Apply the camera's properties to context, including matrix as well as some other properties
         context.SetupCameraProperties(camera);
+        CameraClearFlags flags = camera.clearFlags;
         //Clear the earlier render target
         //TODO: what will happen if remove this
-        _buffer.ClearRenderTarget(true, true, Color.clear);
-        
-        _buffer.BeginSample(_bufferName);
+        _buffer.ClearRenderTarget(flags <= CameraClearFlags.Depth, flags <= CameraClearFlags.Color, flags == CameraClearFlags.Color ? camera.backgroundColor.linear : Color.clear);
+
+        _buffer.BeginSample(SampleName);
         ExecuteBuffer();
     }
+
     void DrawVisibleGeometry()
     {
         var sortSettings = new SortingSettings(camera)
         {
-            criteria = SortingCriteria.CommonOpaque 
+            criteria = SortingCriteria.CommonOpaque
         };
         var drawingSettings = new DrawingSettings(_unlitShaderTagId, sortSettings);
         var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
-        
+
         //Draw opaque first, then the skybox, finally transparent things
         context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
 
         context.DrawSkybox(camera);
-        
+
         sortSettings.criteria = SortingCriteria.CommonTransparent;
         drawingSettings.sortingSettings = sortSettings;
         filteringSettings.renderQueueRange = RenderQueueRange.transparent;
-        
+
         context.DrawRenderers(_cullingResults, ref drawingSettings, ref filteringSettings);
-        
     }
-
-
 
 
     /// <summary>
@@ -80,7 +82,7 @@ public partial class CameraRenderer
     /// </summary>
     void Submit()
     {
-        _buffer.EndSample(_bufferName);
+        _buffer.EndSample(SampleName);
         ExecuteBuffer();
         context.Submit();
     }
@@ -104,6 +106,3 @@ public partial class CameraRenderer
         return false;
     }
 }
-
-
-   
